a30727d97010920e7d67891fe7a07c250f644044
moonknows <huangliying@huangliyingdeMacBook-Pro.local>
2021-08-31 22:12:54 +0800

    update example

--- examples/export.py
+++ examples/export.py
@@ -4,8 +4,8 @@ from resworb.browsers.safari import Safari
 
 safari = Safari()
 
-print(safari.export(kinds=["cloud_tabs"]))
-print(safari.export(kinds=["readings"]))
-print(safari.export(kinds=["bookmarks"]))
-print(safari.export(kinds=["histories"]))
+print(safari.export(kinds="cloud_tabs"))
+print(safari.export(kinds="readings"))
+print(safari.export(kinds="bookmarks"))
+print(safari.export(kinds="histories"))
 print(safari.export(kinds="all"))
